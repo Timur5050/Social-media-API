566174def019b443b126cc75c59ae6168a7cc053
timur <stukantimur811@gmail.com>
2024-08-08 23:25:54 +0300

    created post and comment models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
