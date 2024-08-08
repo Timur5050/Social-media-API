3a5bed4e36c1d75a8b6f2c4109400659a3c25cdc
timur <stukantimur811@gmail.com>
2024-08-08 11:04:50 +0300

    added username as a not required field

--- users/models.py
+++ users/models.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
+from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext as _
+
+
+@deconstructible
+class UnicodeUsernameValidator(validators.RegexValidator):
+    regex = r"^[\w.@+-]+\Z"
+    message = _(
+        "Enter a valid username. This value may contain only letters, "
+        "numbers, and @/./+/-/_ characters."
+    )
+    flags = 0
 
 
 class UserManager(BaseUserManager):
@@ -36,6 +48,20 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+        null=True,
+        blank=True
+    )
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
