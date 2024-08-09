3a37af5ccf876ea7c8b00f6764a858232c2d57e0
timur <stukantimur811@gmail.com>
2024-08-09 14:44:39 +0300

    added likes, comments and images to serializers

--- posts/serializers.py
+++ posts/serializers.py
@@ -15,24 +15,52 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class CommentListSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+
     class Meta:
         model = Comment
         fields = ("author", "created", "text")
 
 
 class PostSerializer(serializers.ModelSerializer):
+    count_of_comments = serializers.IntegerField(source="comments.count", read_only=True)
+    count_of_likes = serializers.IntegerField(source="likes.count", read_only=True)
+
     class Meta:
         model = Post
-        fields = ("id", "title", "description", "created")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "created",
+            "count_of_comments",
+            "count_of_likes"
+        )
 
     def create(self, validated_data):
-        return Post.objects.create(**validated_data, author=self.context["request"].user)
+        return Post.objects.create(
+            **validated_data,
+            author=self.context["request"].user
+        )
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
 
 
 class PostRetrieveSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
     comments = CommentListSerializer(many=True, read_only=True)
+    likes = serializers.SlugRelatedField(slug_field="author.username", read_only=True, many=True)
 
     class Meta:
         model = Post
-        fields = ("id", "title", "description", "author", "created", "comments")
+        fields = (
+            "id",
+            "title",
+            "description",
+            "author",
+            "created",
+            "comments",
+            "likes",
+            "post_image"
+        )
