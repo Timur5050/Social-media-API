9131894418ebd8f71e2e734886a036ac42a24bac
timur <stukantimur811@gmail.com>
2024-08-09 14:45:18 +0300

    added url patterns to like, unlike posts and see your liked posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,10 @@ from rest_framework import routers
 from posts.views import (
     PostViewSet,
     MyPostsViewSet,
-    create_comment
+    create_comment,
+    like_post,
+    unlike_post,
+    get_liked_posts
 )
 
 router1 = routers.DefaultRouter()
@@ -18,6 +21,9 @@ urlpatterns = [
     path("", include(router1.urls)),
     path("", include(router2.urls)),
     path("comment/<int:pk>/", create_comment, name="comment_post"),
+    path("like/<int:pk>/", like_post, name="like_post"),
+    path("unlike/<int:pk>/", unlike_post, name="unlike_post"),
+    path("liked/", get_liked_posts, name="liked-posts")
 ]
 
 app_name = "posts"
