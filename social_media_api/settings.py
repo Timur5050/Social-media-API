3f1f479aa601e310049cfe14f63eb0e064e42824
timur <stukantimur811@gmail.com>
2024-08-09 14:46:36 +0300

    added default permission class: isAuthenticated

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -129,7 +129,10 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
 }
 
 SIMPLE_JWT = {
